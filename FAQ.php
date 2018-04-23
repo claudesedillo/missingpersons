bef12c4e24e2ad0d043cb8d0d0188598b42ae98b
Harvey Lallave <harvey_lallave@gmail.com>
2018-04-23 10:45:12 +0800

    FAQ Page

---
+++ FAQ.php
@@ -0,0 +1,35 @@
+<?php
+	/* Template Name: 9 - FAQ
+	 */
+	get_header();
+?>
+
+<div class = 'container'>
+	<ul class="breadcrumb">
+		<li><a href="#">About</a></li>
+		<li><a href="#">FAQ</a></li>
+	</ul>
+	
+	<h1><b>FREQUENTLY ASKED QUESTIONS</b></h1>
+
+	<div class = 'container'>
+		<button class="accordion">Question 1</button>
+			<div class="panel">
+			<p>Lorem ipsum...</p>
+		</div>
+
+		<button class="accordion">Question 2</button>
+			<div class="panel">
+			<p>Lorem ipsum...</p>
+		</div>
+
+		<button class="accordion">Question 3</button>
+			<div class="panel">
+			<p>Lorem ipsum...</p>
+		</div>
+	</div>
+</div>
+
+<?php
+	get_footer();
+?>
