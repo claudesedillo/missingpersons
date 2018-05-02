66e93599e417db693472e0de34b718ad8e37a1a8
claudesedillo <claudesedillo@gmail.com>
2018-05-02 13:56:56 +0800

    dynamic user dashboard
    - the userID is still hardcoded, this should be taken from a cookie

--- 6B - User Dashboard.php
+++ 6B - User Dashboard.php
@@ -1,4 +1,5 @@
 <?php
+    setcookie("logged", "claude");
 	/* Template Name: 6 - User Dashboard
 	 */
 	get_header();
@@ -18,43 +19,67 @@
 	<div class="tab-content">
 		<div class="tab-pane active text-style" id="tab1">
 			<h2>My Case/s</h2>
+                <?php
+                    global $wpdb;
+                    global $userid; //get from cookie
+                    global $username;
+                    $username = $wpdb->get_results ( "SELECT * FROM users WHERE userID=1");
+            echo $wpdb->last_query;
+                    $result = $wpdb->get_results ( "SELECT * FROM mp_casedetails WHERE userid=1");
+                    
+                    foreach ( $result as $mycase )   {
+                ?>
 				<div class="row">
-					<div class="col-lg-8 panel panel-default">
-						<p><h3>Jordan Gana</h3><p>
-						<a href="" class="underlined">Lorem lorem</a>
-					</div>
-					<div class="col-lg-8 panel panel-default">
-						<h3>Jess Gano</h3>
-						<a href="" class="underlined">Lorem lorem</a>
-					</div>
-					<div class="col-lg-8 panel panel-default">
-						<p><h3>Claude Sedillo</h3><p>
-						<a href="" class="underlined">Lorem lorem</a>
-					</div>
-					<div class="col-lg-8 panel panel-default">
-						<h3>Harvey Lallave</h3>
-						<a href="" class="underlined">Lorem lorem</a>
+					<div class="col-lg-8 panel panel-default">   
+						<p><h3><?php echo $mycase->threadtitle; ?></h3><p>
+						<a href="" class="underlined"><?php echo $username->username; ?></a>
+
 					</div>
 				</div>
+                <?php
+                } 
+                ?>
 			<hr>
 		</div>
 		<div class="tab-pane text-style" id="tab2">
 			<h2>Pinned Cases</h2>
-			<div class="row">
-				<div class="col-lg-8 panel panel-default">
-					<p><h3>Jordan Gana</h3><p>
-					<a href="" class="underlined">Lorem lorem</a>
+            <?php
+                    global $wpdb;
+                    global $userid; //get from cookie
+                    $result = $wpdb->get_results ( "SELECT * FROM mp_pinnedcases WHERE userid=1");
+                    foreach ( $result as $pinnedcase )   {
+                ?>
+				<div class="row">
+					<div class="col-lg-8 panel panel-default">
+                        <?php 
+                            $caseid = $pinnedcase->caseid;
+                            $casedetails = $wpdb->get_results ( "SELECT * FROM mp_casedetails WHERE id='$caseid'");
+                            foreach ( $casedetails as $casedetails )   {
+                        ?>    
+						<p><h3><?php echo $casedetails->threadtitle; ?></h3><p>
+                        <?php
+                                $userid = $casedetails->userID;
+                            }
+                            $threadposter = $wpdb->get_results ( "SELECT * FROM users WHERE userID=$userid");
+                            foreach ( $threadposter as $threadposter )   {
+                        ?>
+						<a href="" class="underlined"><?php echo $threadposter->username; ?></a>
+                        <?php
+                        } 
+                    ?>
+					</div>
+                    <?php
+                            }
+                        ?>
 				</div>
-				<div class="col-lg-8 panel panel-default">
-					<h3>Claude Sedillo</h3>
-					<a href="" class="underlined">Lorem lorem</a>
-				</div>
-			</div>
 			<hr>
 		</div>
 		<div class="tab-pane text-style" id="tab3">
 			<h2>Inbox</h2>
 			<div class="row">
+                <?php
+            
+                ?>
 				<div class="col-lg-8 panel panel-default ">
 					<p><h3>Subject: Lorem Ipusum</h3><p>
 					<a href="" class="underlined">Lorem lorem</a>
