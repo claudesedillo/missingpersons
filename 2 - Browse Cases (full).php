a8851d8446ccffda04c3cf89c4d2f9ee60140522
Harvey Lallave <harvey_lallave@gmail.com>
2018-05-03 13:51:42 +0800

    Optimized code

--- 2 - Browse Cases (full).php
+++ 2 - Browse Cases (full).php
@@ -9,48 +9,59 @@
 	global $wpdb; 
 	global $type;
 	$type = $_GET['type'] == null ? "unsolved" : $_GET['type'];
-	$result = $wpdb->get_results("SELECT * FROM casedetails WHERE ".
-								 "status = '$type' ORDER BY lastseen DESC");
+	$result = $wpdb->get_results("SELECT *, (SELECT COUNT(*) FROM 
+											 threadpost WHERE postId = id) AS n   
+								  FROM casedetails WHERE status = '$type' 
+								  ORDER BY lastseen DESC, datePosted DESC 
+								  LIMIT 4", ARRAY_A);
 	if(strcmp($type, "unsolved") == 0){
 ?>
 		<div class="container-fluid"> 
 			<div class="row maindivs" id="browsepics-div">
 				<h4><?php echo strtoupper($type)?> CASES</h4>
 <?php
-	for($i = 0; $i < 4; $i++){
-        $caseNumber = $i + 1;
+	foreach($result as $row){
         $src = "http://wordpress.local/5-specific-case?caseNumber={$caseNumber}";
 ?>      
 				<div class="col-sm-3">
-                    <a href = "<?php echo $src ?>"><img class=" img-responsive" src="../wp-content/themes/missingPersons/images/icon.png"></a>
-					<p>Last Location: <?php echo $result[$i]->lastlocation;?></p>
-					<p>Last seen: <?php echo $result[$i]->lastseen;?></p>
+                    <a href = "<?php echo $src ?>">
+						<img class=" img-responsive" 
+							 src="../wp-content/themes/missingPersons/images/icon.png"></a>
+					<p>Last Location: <?php echo $row['lastlocation'];?></p>
+					<p>Last seen: <?php echo $row['lastseen'];?></p>
 				</div>
 <?php  }
 ?>
 				<br><br>
 				<button class="btn btn-default" id="btn-viewallBP"><a href = "http://wordpress.local/5-specific-case?type={unsolved}">VIEW ALL</a></button>
 			</div>
-<?php	}
+<?php }
 ?>
 			<div class="row maindivs" id="unsolved-div"> 
 				<div class="row headers">
 					<h4><?php echo strtoupper($type) ?> CASES</h4>
 				</div>
 <?php
-	for($i = 0; $i < 5; $i++){
-        $caseNumber = $i + 1;
-        $src = "http://wordpress.local/5-specific-case?caseNumber={$caseNumber}";
+
+	$result = $wpdb->get_results("SELECT *, (SELECT COUNT(*) FROM 
+											 threadpost WHERE postId = id) AS n   
+								  FROM casedetails WHERE status = '$type' 
+								  ORDER BY lastseen DESC, datePosted DESC 
+								  LIMIT 4", ARRAY_A);
+	foreach($result as $row){
+        $src = "http://wordpress.local/5-specific-case?caseNumber={$row['id']}";
 ?>   
 				<div class="row" id="report-div">
 					<div class="col-sm-8">
-						<p><a href = "<?php echo $src ?>">[<?php echo strtoupper($result[$i]->status) ?>] 
-                            <?php echo strtoupper($result[$i]->lName) ?>, <?php echo strtoupper($result[$i]->fName) ?></a></p>
-						<p>Date poster: March 23, 2018</p>
-						<p>Replies: 10</p>
+						<p><a href = "<?php echo $src ?>">
+						   [<?php echo strtoupper($row['status']) ?>] 
+                            <?php echo strtoupper($row['lName']) ?>, 
+							<?php echo strtoupper($row['fName']) ?></a></p>
+						<p>Date poster: <?php echo $row['datePosted'] ?></p>
+						<p>Replies: <?php echo $row['n'] ?></p>
 					</div>
 					<div class="col-sm-4">
-						<p>Location <br> Taguig</p>
+						<p>Location <br> <?php echo $row['lastlocation'] ?></p>
 					</div>
 				</div><hr>
 <?php  }
