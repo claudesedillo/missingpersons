d3f8afe1e5178fab1de3404955e7652e3c8656b9
Harvey Lallave <harvey_lallave@gmail.com>
2018-05-04 17:09:23 +0800

    Merged UI

--- functions.php
+++ functions.php
@@ -9,19 +9,32 @@
 			{
 				case '1-home':
 					wp_enqueue_script('home', get_template_directory_uri() . '/js/home.js', array('jquery'), '', false);
-                    wp_enqueue_style( '1.css', get_template_directory_uri() . '/css/1.css' );
+                    wp_enqueue_style( '1home.css', get_template_directory_uri() . '/css/1home.css' );
+                    break;
+				case '2-browse-cases-full':
+                    wp_enqueue_style( '2browsecasesfull.css', get_template_directory_uri() . '/css/2browsecasesfull.css' );
                     break;
                 case '3-browse-cases-with-pictures':
-                    wp_enqueue_style( '3.css', get_template_directory_uri() . '/css/3.css' );                   
+                    wp_enqueue_style( '3browsecasespics.css', get_template_directory_uri() . '/css/3browsecasespics.css' );                   
                     wp_enqueue_script('browseCase.js', get_template_directory_uri() . '/js/browseCase.js', array('jquery'), '', false);
                     break;
-                case '5-specific-case':
+				case '4-search-page':
+					wp_enqueue_style( 'search-page', get_template_directory_uri() . '/css/search-page.css' );
+                    break;
+				case '5-specific-case':
                     wp_enqueue_script('specific-case', get_template_directory_uri() . '/js/sc.js', array('jquery'), '', false);
-                    wp_enqueue_style( '5.css', get_template_directory_uri() . '/css/5.css' );
-                    break;    
+                    wp_enqueue_style( '5specificcase.css', get_template_directory_uri() . '/css/5specificcase.css' );
+                    break; 
+                case '6-user-dashboard':
+                    wp_enqueue_style( 'dashboard', get_template_directory_uri() . '/css/dashboard.css' );
+                    break;					
                 case '7-submit-report':
 					wp_enqueue_script('submitReport.js', get_template_directory_uri() . '/js/submitReport.js', array('jquery'), '', false);
-					break;    
+					break;   
+                case '8-about':
+                    wp_enqueue_style( '8about', get_template_directory_uri() . '/css/8about.css' );
+                    break;                
+				
 				case 'some-post':
 					wp_enqueue_script('somepost', get_template_directory_uri() . '/js/somepost.js', array('jquery'), '1.6', true);
 					break;
