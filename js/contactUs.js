a9d6fc0edd47e6eef0381eb7348e07f933e7d219
Harvey Lallave <harvey_lallave@gmail.com>
2018-05-07 11:40:11 +0800

    Input validation #8 Contact Us

--- js/contactUs.js
+++ js/contactUs.js
@@ -3,20 +3,26 @@ $("document").ready(function(){
 	
 	$('#contactUsForm').on('submit', function (e) {
 		e.preventDefault();
-		$.ajax({
-			type: 'post',
-			url: "../wp-content/themes/missingPersons/methods.php",
-			data: $('form').serialize() + "&action=contactUsForm",
-			success: function (e) {
-				alert(e);
-			},
-			error: function () {
-				alert("Database Insertion Failed");
-			},
-			complete: function () {
-				$("#contactUsForm")[0].reset();
-			}
-		});
+		
+		if($('#name').val() != null    && $('#name').val()    != "" &&
+		  ($('#email').val() != null   && $('#email').val()   != "" ||
+		   $('#number').val() != null  && $('#number').val()  != "")&&
+		   $('#message').val() != null && $('#message').val() != ""){
+			$.ajax({
+				type: 'post',
+				url: "../wp-content/themes/missingPersons/methods.php",
+				data: $('form').serialize() + "&action=contactUsForm",
+				success: function (e) {
+					alert(e);
+				},
+				error: function () {
+					alert("Database Insertion Failed");
+				},
+				complete: function () {
+					$("#contactUsForm")[0].reset();
+				}
+			});
+	   } else alert("Please fill up the whole form with one means of contact");
 
 	});
 });
